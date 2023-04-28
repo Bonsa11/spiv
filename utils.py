6556c8639827af0ea693cc4901fdd76fd4eac63d
Bonsa11 <sambodza@hotmail.co.uk>
2023-04-28 11:28:58 +0100

    updatedpath,lgoic

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 def parse_folder_path(fldr: str):
-    if 'uhb/appdata' in fldr:
-        if 'uhb/appdata/leicabs' in fldr:
-            return fldr.replace('\\','/').replace(r'//uhb/appdata/leicabs','/mnt/uhb/appdata/leicabs')
-        elif 'uhb/userdata/Pathology' in fldr:
-            return fldr.replace('\\','/').replace(r'//uhb/userdata/Pathology','/mnt/uhb/userdata/Pathology')
+    pfldr = fldr.replace('\\','/')
+    if 'uhb/appdata' in pfldr:
+        if 'uhb/appdata/leicabs' in pfldr:
+            return pfldr.replace(r'//uhb/appdata/leicabs','/mnt/uhb/appdata/leicabs')
+        elif 'uhb/userdata/Pathology' in pfldr:
+            return pfldr.replace(r'//uhb/userdata/Pathology','/mnt/uhb/userdata/Pathology')
 
-    if '10.156.76.199' in fldr:
-        return fldr.replace('\\','/').replace(r'//10.156.76.199/molecular_pathology_imaging','/mnt/molecular_pathology_imaging/')
+    if '10.156.76.199' in pfldr:
+        return pfldr.replace(r'//10.156.76.199/molecular_pathology_imaging','/mnt/molecular_pathology_imaging/')
 
     else:
         return fldr
