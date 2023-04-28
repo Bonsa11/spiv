e1313a0f1f3f5dc870c72bb8ef4a6918fa3384ae
Bonsa11 <sambodza@hotmail.co.uk>
2023-04-28 11:31:18 +0100

    updatedpath,lgoic

--- utils.py
+++ utils.py
@@ -1,12 +1,13 @@
 def parse_folder_path(fldr: str):
     pfldr = fldr.replace('\\','/')
-    if 'uhb/appdata' in pfldr:
-        if 'uhb/appdata/leicabs' in pfldr:
-            return pfldr.replace(r'//uhb/appdata/leicabs','/mnt/uhb/appdata/leicabs')
-        elif 'uhb/userdata/Pathology' in pfldr:
-            return pfldr.replace(r'//uhb/userdata/Pathology','/mnt/uhb/userdata/Pathology')
 
-    if '10.156.76.199' in pfldr:
+    if 'uhb/appdata/leicabs' in pfldr:
+        return pfldr.replace(r'//uhb/appdata/leicabs','/mnt/uhb/appdata/leicabs')
+    
+    elif 'uhb/userdata/Pathology' in pfldr:
+        return pfldr.replace(r'//uhb/userdata/Pathology','/mnt/uhb/userdata/Pathology')
+
+    elif '10.156.76.199' in pfldr:
         return pfldr.replace(r'//10.156.76.199/molecular_pathology_imaging','/mnt/molecular_pathology_imaging/')
 
     else:
