5565b6313d658db09d0d1f571b746745641cb4cd
Bonsa11 <sambodza@hotmail.co.uk>
2023-04-28 11:09:24 +0100

    added new path

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 def parse_folder_path(fldr: str):
-    if 'uhb/appdata/leicabs' in fldr:
-        return fldr.replace('\\','/').replace(r'//uhb/appdata/leicabs','/mnt/uhb/')
+    if 'uhb/appdata' in fldr:
+        if 'uhb/appdata/leicabs' in fldr:
+            return fldr.replace('\\','/').replace(r'//uhb/appdata/leicabs','/mnt/uhb/appdata/leicabs')
+        elif 'uhb/userdata/Pathology' in fldr:
+            return fldr.replace('\\','/').replace(r'//uhb/userdata/Pathology','/mnt/uhb/userdata/Pathology')
 
     if '10.156.76.199' in fldr:
         return fldr.replace('\\','/').replace(r'//10.156.76.199/molecular_pathology_imaging','/mnt/molecular_pathology_imaging/')
